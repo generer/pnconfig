777769eadcee1bd95e59d465eb5a5cc3f4e64b65
jtj@post.dk <jtj@post.dk>
2019-06-12 14:12:47 +0200

    thisisachane

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var Error = require('./errorhandler.js')
 var Util = require('./util.js');
 var Autocomplete = require('./autocomplete.js');
 
-
+var thisisatest = false;
 
 module.exports = {"Ajax":Ajax,"Error":Error,"Util":Util,"Autocomplete":Autocomplete}
